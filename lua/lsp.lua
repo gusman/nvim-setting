5ac5106bb1c450f01f67223b16cd77bd8609edcb
Gusman Dharma P <gusman.dp@gmail.com>
2024-10-18 22:41:29 +0700

    Enable lsp single line

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -7,7 +7,7 @@ lspconfig.gopls.setup({
 	cmd = { "gopls" },
 	filetypes = { "go", "gomod", "gowork", "gotmpl" },
 	root_dir = util.root_pattern( "go.work", "go.mod", ".git" ),
-	single_file_support = false,
+	single_file_support = true,
 	settings = {
 		gopls = {
 			analyses = {
@@ -23,7 +23,7 @@ require'lspconfig'.pylsp.setup{
 	cmd = { "pylsp" },
 	filetypes = { "python" },
 	root_dir = util.root_pattern( ".git", "setup.cfg", "setup.py", "pyproject.toml" ),
-	single_file_support = false,
+	single_file_support = true,
 	settings = {
 		pylsp = {
 			plugins = {
